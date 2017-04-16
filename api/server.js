2fbfb3b36589d6f6408e37701d8cd845e750362b
HUQ <jhuq@linkedin.com>
2017-04-15 21:09:01 -0700

    blog component pathway created

--- api/server.js
+++ api/server.js
@@ -68,6 +68,13 @@ for (var s in config) {
   passportConfigurator.configureProvider(s, c);
 }
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+});
 
 app.post('/signup', function(req, res, next) {
   var User = app.models.user;
