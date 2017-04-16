f25bc8aebceba46049fa7d08c7bfa0996cadd0f5
HUQ <jhuq@linkedin.com>
2017-04-16 00:53:49 -0700

    blog url data is pooped onto page

--- api/boot/blog-proxy.js
+++ api/boot/blog-proxy.js
@@ -3,16 +3,22 @@ var request = require('request');
 module.exports = function(server) {
   server.get('/api/blog', (req, res) => {
     var url = req.query.url;
+
     if (!url) return res.status(500).send({
       status: 'error',
       message: 'No url found!'
     });
+
     request(url, function (error, response, body) {
-      if (error || response.statusCode != 200) return res.status(500).send({
-        status: 'error',
-        message: "OH GOD – CAN'T RESOLVE URL!"
-      });
-      res.send(body);
+      if (error) {
+        return res.status(500).send({
+          status: 'error', message: "OH GOD – CAN'T RESOLVE URL!"
+        });
+      }
+
+      if (!error && response.statusCode == 200) {
+        res.send(body);// Print the google web page.
+      }
     });
  });
 };
--- app/components/blog-widget.js
+++ app/components/blog-widget.js
@@ -1,25 +1,44 @@
 import Ember from 'ember';
+const { get, set, run, $, RSVP } = Ember;
 
-// function ajax (url, options) {
-//   return new Ember.RSVP.Promise(function (resolve, reject) {
-//     options = options || {};
-//     options.url = url;
-//
-//     options.success = function (data) {
-//       Ember.run(null, resolve, data);
-//     };
-//
-//     options.error = function (jqxhr, status, something) {
-//       Ember.run(null, reject, arguments);
-//     };
-//
-//     Ember.$.ajax(options);
-//   });
-// }
+function ajax (url, options) {
+  return new RSVP.Promise(function (resolve, reject) {
+    options = options || {};
+    options.url = url;
+
+    options.success = function (data) {
+      run(null, resolve, data);
+    };
+
+    options.error = function (jqxhr, status, something) {
+      run(null, reject, arguments);
+    };
+
+    $.ajax(options);
+  });
+}
+
+function formatBlogData(host, response) {
+  switch(host) {
+    case "medium":
+      var responseWierdness =  '])}while(1);</x>';
+      var parsedResponse = response.slice(responseWierdness.length);
+      console.log("medium!");
+      return parsedResponse;
+    case "square":
+      console.log("square!");
+      return response;
+    default:
+      console.log("create a blog!");
+      return response;
+  }
+}
+
+const listOfRecentPostBlogUrl = encodeURIComponent("https://medium.com/@bryantaxs/latest?format=json");
 
 export default Ember.Component.extend({
-  // title: "Blog",
-  // Window.fetch
-  // store: ajax("https://www.marchforscience.com/blog?format=json"),
-  // model: this.store.findAll()
+  init() {
+    this._super(...arguments);
+    ajax(`/api/blog?url=${listOfRecentPostBlogUrl}`).then((res) => set(this, 'recentBlogPosts', formatBlogData('medium', res)));
+  },
 });
