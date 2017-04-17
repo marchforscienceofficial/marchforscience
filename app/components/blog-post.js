de5ce12bcb59626df112e4d1fcd3593e3e49689c
HUQ <jhuq@linkedin.com>
2017-04-17 12:15:22 -0700

    date of post shows as slashed value order based on l18n

--- app/components/blog-post.js
+++ app/components/blog-post.js
@@ -3,5 +3,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   linkToPost: Ember.computed('post', 'blogUrl', function() {
     return `${this.get('blogUrl')}${this.get('post.uniqueSlug')}`
+  }),
+  displayPostDate: Ember.computed('post', function() {
+    const date = new Date(this.get('post.firstPublishedAt'));
+    return new Intl.DateTimeFormat().format(date) ;
   })
 });
