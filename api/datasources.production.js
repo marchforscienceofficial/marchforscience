e352f2ef66d3cb016c542bebb9a5ca2abd84c156
Eddie Kay <ichthyos@gmail.com>
2017-04-22 06:38:25 -0700

    Remove MongoDB from production

--- api/datasources.production.js
+++ api/datasources.production.js
@@ -3,15 +3,5 @@ module.exports = {
     "name": "gcloud-datastore",
     "connector": "gcloud",
     "projectId": process.env.GOOGLE_PROJECT_ID,
-  },
-  "mongodb": {
-    "connector": "mongodb",
-    "name": "mongodb",
-    "host": process.env.MONGO_HOST,
-    "database": "marchforscience",
-    "url":  "",
-    "user": "marchforscience",
-    "password": process.env.MONGO_PASS,
-    "port": 27017
   }
 };
