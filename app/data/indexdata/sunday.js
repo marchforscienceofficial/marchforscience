11d45aa2312ea2164d4b9dd8f882f31267999a99
Adam Miller <adam@pages.me>
2017-04-23 03:43:27 -0700

    Make responsive

--- app/data/indexdata/sunday.js
+++ app/data/indexdata/sunday.js
@@ -7,21 +7,18 @@ export default  {
   //************************/
   CTAs: [{
     className: 'leftCall',
-    heading: 'join the science network',
-    body: 'This is just the beginning. Many Satellites are signing on to become a permanent part of the March for Science network.'
-    + ' You can create an account and register to get updates nationwide and in your local community.'
-    + ' Keep an eye on your Satellite’s homepage; we’re rolling out exciting new features to keep you informed and connected in the coming weeks!',
+    heading: 'Join the movement',
+    body: 'Yesterday, over 600 cities marched worldwide. Many of these Satellites are signing on to become a permanent part of the March for Science network. To support these Satellites, we’re designing infrastrucutre and resources to connect communities to coordinated actions, needs, and resources. Keep an eye on your Satellite’s homepage; we’re rolling out exciting new features in the coming weeks.\n\nOnce you’ve signed up, recruit a few friends to do the same!',
     buttons: [
-      {text: "browse satellites", href: "satellites"}
+      {text: "Browse satellites", href: "satellites"}
     ],
     image: "images/icon-globe.svg",
   }, {
     className: 'rightCall',
-    heading: 'Listen to the post-march podcast',
-    body: 'March for Science and the Taste of Science are co-hosting a podcast at the Carnegie Institute. '
-    + 'Tune in to listen to organizers and community members share stories from the March and their vision for the future.',
+    heading: 'Learn about what comes next',
+    body: 'This is just the beginning. We are building an organization centered on informed advocacy, community building, and accessible education. We’ll work with Satellites and partners to create new programs and scale existing programs aimed at improving the relationship between science and society.',
     buttons: [
-      {text: 'listen now', href: ''},
+      {text: 'Learn more', href: 'vision'},
     ],
     image: "images/icon-bulb.svg",
   }],
@@ -30,12 +27,11 @@ export default  {
   //**     Megaphone & Twitter           **//
   //***************************************/
   megaphone: {
-    text: "Contact your local officials (council members,"
-    + " your mayor, county executives) with an MFS postcard explaining why they should support scientific institutions and science-based policies.",
+    text: "Read and share \"I Marched For Science,\" which includes a statement on the importance of civic engagement.",
     hash: "#NoSidesInScience",
-    button: {text: "contact now", href: ""},
+    button: {text: "Read now", href: ""},
     twitterHash: '#KEEPMARCHING',
-    twitterButton: {text: "Tell us how YOU marched for science.", href:""},
+    twitterButton: {text: "Tell us how YOU marched for science.", href:"http://twitter.com/home?status=%23keepmarching%20%23marchforscience"},
     twitterSplain: "On April 22, 2017, over 500 Marches worldwide stood up for science. Today is all about celebrating the accomplishments of local communities, pushing the conversation forward,"
     + " and reflecting on the power of this movement. Post pictures of where you marched or the sign you held.",
     twitterTag: "Share your favorite experiences, stories of the people you met, the things you learned, or what you’re inspired to do next.",
@@ -45,25 +41,26 @@ export default  {
     //**     More Ways to Get Involved     **//
     //***************************************/
   moreCTAs: [{
+      header: "Learn how to give a talk in your local community",
+      group: "American Geophysical Union",
+      bodyWithHTMLLinks: "Reaching out to local groups is a great way to share your science"
+      + " and explain why it’s relevant and important to your community."
+      + " View AGU's resources at <a href='http://www.defenders.org/ten-things-you-can-do-help' target='_blank'>www.defenders.org</a>.",
+      image: 'images/moreCTA/icon-talk.svg',
+    },{
       header: "Defend the role of science in a new political era - An open letter to President Trump and the 115th Congress",
       group: "Union of Concerned Scientists",
       bodyWithHTMLLinks: "Join Nobel Laureates, prominent scientists, "
       + "and fellow experts on an open letter outlining expectations"
       + " for the use of science in the Trump administration at "
-      + "<a href='http://www.ucsusa.org/center-science-and-democracy/tweet-your-senator-protect-science-based-safeguards?autologin=true#.WPKHQVMrIxF'>www.ucsusa.org</a>.",
+      + "<a href='http://www.ucsusa.org/center-science-and-democracy/tweet-your-senator-protect-science-based-safeguards?autologin=true#.WPKHQVMrIxF'  target='_blank'>www.ucsusa.org</a>.",
       image: 'images/moreCTA/journal-01.svg',
     }, {
       header: "Join a science teach-in!",
       group: "National Center for Science Education (NCSE)",
       bodyWithHTMLLinks: "NCSE has developed Science Teach-In Toolkits to help scientists share knowledge and raise awareness about science and its value to society."
-      + " View the toolkit at <a href='http://sharingscience.agu.org/ways-to-reach-out/'>sharingscience.agu.org</a>.",
+      + " View the toolkit at <a href='http://sharingscience.agu.org/ways-to-reach-out/' target='_blank'>sharingscience.agu.org</a>.",
       image: 'images/moreCTA/apple.svg',
-    }, {
-      header: "Learn how to give a talk in your local community",
-      group: "American Geophysical Union",
-      bodyWithHTMLLinks: "Reaching out to local groups is a great way to share your science"
-      + " and explain why it’s relevant and important to your community."
-      + " View AGU's resources at <a href='http://www.defenders.org/ten-things-you-can-do-help'>www.defenders.org</a>.",
-      image: 'images/moreCTA/icon-talk.svg',
-    }],
+    },
+  ],
 }
