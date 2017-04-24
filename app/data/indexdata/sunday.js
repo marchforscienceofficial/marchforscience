56e255a09729e4afcee0012ffb37582124235fbb
HUQ <jhuq@linkedin.com>
2017-04-23 18:04:21 -0700

    monday & tuesday content up! n

--- app/data/indexdata/sunday.js
+++ app/data/indexdata/sunday.js
@@ -8,15 +8,22 @@ export default  {
   CTAs: [{
     className: 'leftCall',
     heading: 'Join the movement',
-    body: 'Yesterday, over 600 cities marched worldwide. Many of these Satellites are signing on to become a permanent part of the March for Science network. To support these Satellites, we’re designing infrastrucutre and resources to connect communities to coordinated actions, needs, and resources. Keep an eye on your Satellite’s homepage; we’re rolling out exciting new features in the coming weeks.\n\nOnce you’ve signed up, recruit a few friends to do the same!',
+    body: 'Yesterday, over 600 cities marched worldwide. '
+    + 'Many of these Satellites are signing on to become a permanent part of the March for Science network.'
+    + ' To support these Satellites, we’re designing infrastrucutre and resources to connect communities to coordinated actions, needs,'
+    + ' and resources. Keep an eye on your <a href="https://satellites.marchforscience.com/satellites">Satellites</a> homepage;'
+    + ' we’re rolling out exciting new features in the coming weeks.\n\nOnce you’ve signed up, recruit a few friends to do the same!',
     buttons: [
-      {text: "Browse satellites", href: "satellites"}
+      {text: "Browse satellites", href: "https://satellites.marchforscience.com/satellites"}
     ],
     image: "images/icon-globe.svg",
   }, {
     className: 'rightCall',
     heading: 'Learn about what comes next',
-    body: 'This is just the beginning. We are building an organization centered on informed advocacy, community building, and accessible education. We’ll work with Satellites and partners to create new programs and scale existing programs aimed at improving the relationship between science and society.',
+    body: 'This is just the beginning.'
+    + ' We are building an organization centered on informed advocacy,'
+    + ' community building, and accessible education. We’ll work with Satellites and partners to create new programs and scale existing programs aimed at'
+    + ' improving the relationship between science and society.',
     buttons: [
       {text: 'Learn more', href: 'vision'},
     ],
